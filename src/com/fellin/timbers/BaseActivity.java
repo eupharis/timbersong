b7d00ac9583b21167960cccf3cc13866460a8ea9
eupharis <dfellin@gmail.com>
2012-03-30 08:15:50 -0700

    Commiting before undergoing major revisions!

---
+++ src/com/fellin/timbers/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.fellin.timbers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+	//Needs extension
+	@Override	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
--- src/com/fellin/timbers/ScheduleView.java
+++ src/com/fellin/timbers/ScheduleView.java
@@ -1,21 +1,40 @@
 package com.fellin.timbers;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.fellin.timbersong.R;
 
-public class ScheduleView extends ListActivity {
+
+public class ScheduleView extends Activity {
 	
 	@Override	
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-				
-		String[] gameList = getResources().getStringArray(R.array.home_game_array);
-		
-		ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameList);
-		setListAdapter(adapt);
-		
+		try
+		{
+			super.onCreate(savedInstanceState);
+			ListView scheduleList;
+			ArrayAdapter<String> adapt;
+			
+			setContentView(R.layout.schedule_list);
+			scheduleList = (ListView) findViewById(R.id.scheduleList);
+			
+			String[] gameList = getResources().getStringArray(R.array.home_game_array);
+			
+			adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gameList);
+			scheduleList.setAdapter(adapt);
+		}
+		catch (Exception e)
+		{
+			// this is the line of code that sends a real error message to the log
+			Log.e("ERROR", "ERROR IN CODE: " + e.toString());
+	 
+			// this is the line that prints out the location in
+			// the code where the error occurred.
+			e.printStackTrace();
+		}
 	}
 }
