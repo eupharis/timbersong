c5fa296490b971f30acaefc740ca61920e38eae9
Dan Fellin <dan@highwaterlabs.com>
2012-05-12 10:13:49 -0700

    Working File - Pre-XML Hacking

---
+++ gen/com/fellin/timbersong/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fellin.timbersong;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- gen/com/fellin/timbersong/R.java
+++ gen/com/fellin/timbersong/R.java
@@ -23,32 +23,35 @@ public final class R {
         public static final int ic_timbersong=0x7f020002;
     }
     public static final class id {
-        public static final int TabHost1=0x7f080015;
-        public static final int about=0x7f080019;
-        public static final int chantTitleBuryMe=0x7f080005;
-        public static final int chantTitleCantHelp=0x7f080006;
-        public static final int chantTitleLoveYouSo=0x7f08000f;
-        public static final int chantTitleMentalBarmy=0x7f080008;
-        public static final int chantTitleMentalGreen=0x7f080009;
-        public static final int chantTitleMighty=0x7f08000a;
-        public static final int chantTitleNorthEnd=0x7f08000b;
-        public static final int chantTitleOnward=0x7f08000c;
-        public static final int chantTitleParty=0x7f08000d;
-        public static final int chantTitlePortlandBoys=0x7f08000e;
-        public static final int chantTitleSomos=0x7f080011;
-        public static final int chantTitleSunshine=0x7f080014;
-        public static final int chantTitleTetris=0x7f080012;
-        public static final int chantTitleWannaBe=0x7f080007;
-        public static final int chantTitleWhoaOh=0x7f080010;
-        public static final int chantTitleWinTheLeague=0x7f080013;
-        public static final int gameListAway=0x7f080017;
-        public static final int gameListHome=0x7f080016;
-        public static final int name_version=0x7f080000;
-        public static final int scheduleOption=0x7f080018;
-        public static final int scroller=0x7f080001;
-        public static final int showChantLink=0x7f080004;
-        public static final int showChantText=0x7f080003;
-        public static final int showChantTitle=0x7f080002;
+        public static final int TabHost1=0x7f060018;
+        public static final int about=0x7f06001c;
+        public static final int app=0x7f060000;
+        public static final int chantTitleBuryMe=0x7f060008;
+        public static final int chantTitleCantHelp=0x7f060009;
+        public static final int chantTitleLoveYouSo=0x7f060012;
+        public static final int chantTitleMentalBarmy=0x7f06000b;
+        public static final int chantTitleMentalGreen=0x7f06000c;
+        public static final int chantTitleMighty=0x7f06000d;
+        public static final int chantTitleNorthEnd=0x7f06000e;
+        public static final int chantTitleOnward=0x7f06000f;
+        public static final int chantTitleParty=0x7f060010;
+        public static final int chantTitlePortlandBoys=0x7f060011;
+        public static final int chantTitleSomos=0x7f060014;
+        public static final int chantTitleSunshine=0x7f060017;
+        public static final int chantTitleTetris=0x7f060015;
+        public static final int chantTitleWannaBe=0x7f06000a;
+        public static final int chantTitleWhoaOh=0x7f060013;
+        public static final int chantTitleWinTheLeague=0x7f060016;
+        public static final int chants=0x7f060002;
+        public static final int gameListAway=0x7f06001a;
+        public static final int gameListHome=0x7f060019;
+        public static final int name_version=0x7f060003;
+        public static final int scheduleOption=0x7f06001b;
+        public static final int schedules=0x7f060001;
+        public static final int scroller=0x7f060004;
+        public static final int showChantLink=0x7f060007;
+        public static final int showChantText=0x7f060006;
+        public static final int showChantTitle=0x7f060005;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -57,7 +60,7 @@ public final class R {
         public static final int schedule_list=0x7f030003;
     }
     public static final class menu {
-        public static final int options_menu=0x7f070000;
+        public static final int options_menu=0x7f080000;
     }
     public static final class string {
         public static final int about=0x7f040002;
@@ -113,8 +116,8 @@ public final class R {
         public static final int year=0x7f040005;
     }
     public static final class style {
-        public static final int styleButton=0x7f060002;
-        public static final int styleText=0x7f060001;
-        public static final int styleTitle=0x7f060000;
+        public static final int styleButton=0x7f070002;
+        public static final int styleText=0x7f070001;
+        public static final int styleTitle=0x7f070000;
     }
 }
