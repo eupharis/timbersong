43e08ea606cc90d8b4693f2131741cb8301b1cfd
Dan Fellin <dan@highwaterlabs.com>
2012-05-14 14:17:42 -0700

    Updated README with donation link.

---
+++ src/com/fellin/timbers/Chant.java
@@ -0,0 +1,15 @@
+package com.fellin.timbers;
+
+public class Chant {
+	
+	public Chant() {
+		String name = "";
+		String lyrics = "";
+		String video = "";
+	}
+	
+/*	public Chant returnChant() {
+		return this;
+	}*/
+	
+}
